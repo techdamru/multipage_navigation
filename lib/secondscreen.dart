b8a41d34aad12d858fd79e142da9879d297aa5b3
techdamru <techdamru@gmail.com>
2021-08-15 22:53:29 +0530

    Removed button from second screen

--- lib/secondscreen.dart
+++ lib/secondscreen.dart
@@ -1,41 +1,11 @@
 import 'package:flutter/material.dart';
-import 'mainscreen.dart';
 
 class SecondScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(Icons.menu),
-          onPressed: () {
-            //
-          },
-        ),
         title: Text("Second Screen"),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.search),
-            onPressed: () {
-              //
-            },
-          ),
-        ],
-      ),
-      body: Center(
-        child: RaisedButton(
-          child: Text(
-            "Back To Home Screen",
-            style: TextStyle(color: Colors.white),
-          ),
-          color: Colors.indigo,
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => MainScreen()),
-            );
-          },
-        ),
       ),
     );
   }
